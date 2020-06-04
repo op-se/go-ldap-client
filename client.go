f67572878be77ac250cb922ccaffc9737b5d7966
slv922 <slv922@gmail.com>
2020-06-04 17:36:39 +0800

    fix group

--- client.go
+++ client.go
@@ -1,4 +1,4 @@
-package opldap
+package main
 
 import (
 	"crypto/tls"
---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	fmt.Println("hi")
+	s := formatGroup("CN=HQ普噗,OU=HQ豪吃,OU=OPXXX,DC=xxxxx,DC=com,DC=tw")
+
+	fmt.Println(s)
+
+}
